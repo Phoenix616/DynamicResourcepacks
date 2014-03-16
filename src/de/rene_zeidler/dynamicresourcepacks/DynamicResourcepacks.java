a2fc0ada47fcf135e475ab219e62826ee1d89f8d
ReneZeidler <rene.zeidler@gmx.de>
2014-03-16 19:42:05 +0100

    Implemented ResourcepackManager

--- src/de/rene_zeidler/dynamicresourcepacks/DynamicResourcepacks.java
+++ src/de/rene_zeidler/dynamicresourcepacks/DynamicResourcepacks.java
@@ -1,12 +1,19 @@
 package de.rene_zeidler.dynamicresourcepacks;
 
-import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class DynamicResourcepacks extends JavaPlugin {
-
+	ResourcepackManager packManager;
+	
+	@Override
+	public void onLoad() {
+		super.onLoad();
+		
+		this.packManager = new ResourcepackManager(this);
+	}
+	
 	@Override
 	public void onEnable(){
 		this.saveDefaultConfig();
--- src/de/rene_zeidler/dynamicresourcepacks/ResourcepackManager.java
+++ src/de/rene_zeidler/dynamicresourcepacks/ResourcepackManager.java
@@ -1,14 +1,72 @@
 package de.rene_zeidler.dynamicresourcepacks;
 
+import java.util.HashMap;
+
 import org.bukkit.configuration.Configuration;
+import org.bukkit.entity.Player;
 
 public class ResourcepackManager {
-	
 	private DynamicResourcepacks plugin;
 	private Configuration config;
 	
+	private HashMap<Player, String> currentPacks;
+	
+	private static ResourcepackManager instance;
+	
 	public ResourcepackManager(DynamicResourcepacks plugin) {
+		ResourcepackManager.instance = this;
+		
 		this.plugin = plugin;
-		this.config = plugin.getConfig();
+		this.config = this.plugin.getConfig();
+		
+		this.currentPacks = new HashMap<Player, String>();
+	}
+	
+	public static ResourcepackManager getInstance() {
+		return ResourcepackManager.instance;
+	}
+	
+	public String getEmptyPackURL() {
+		return this.config.getString("emptyPackURL");
+	}
+	
+	public void setEmptyPackURL(String url) {
+		this.config.set("emptyPackURL", url);
+	}
+	
+	public void resendAll() {
+		for(Player p : currentPacks.keySet())
+			p.setResourcePack(currentPacks.get(p));
+	}
+	
+	public void resend(Player player) {
+		if(currentPacks.containsKey(player))
+			player.setResourcePack(currentPacks.get(player));
+		else
+			player.setResourcePack(this.getEmptyPackURL());
+	}
+	
+	public void setResourcepack(Player player, String url) {
+		if(url == null) {
+			this.clearResourcepack(player);
+			return;
+		}
+		
+		currentPacks.put(player, url);
+		player.setResourcePack(url);
+	}
+	
+	public void clearResourcepack(Player player) {
+		currentPacks.remove(player);
+		player.setResourcePack(this.getEmptyPackURL());
+	}
+	
+	public String getResourcepack(Player player) {
+		return currentPacks.get(player);
+	}
+	
+	public void clear() {
+		for(Player p : currentPacks.keySet())
+			this.clearResourcepack(p);
 	}
 }
